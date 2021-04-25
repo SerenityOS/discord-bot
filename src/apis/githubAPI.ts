25222460b3a18778b1c7c66d7facf9a9f26a841c
Idan Horowitz <idan.horowitz@gmail.com>
2021-04-25 16:59:23 +0300

    APIs: Add github API helper for searching issues and pull requests
    This also renames the discord token environment variable and adds
    an optional github token environment variable that allows the bot
    to have a higher rate limit by linking it against a github account.

---
+++ src/apis/githubAPI.ts
@@ -0,0 +1,40 @@
+import { Octokit } from "@octokit/rest";
+import { GITHUB_TOKEN } from "../config/secrets";
+
+class GithubAPI {
+    private octokit: Octokit;
+
+    constructor() {
+        this.octokit = new Octokit({
+            userAgent: "BuggieBot",
+            auth: GITHUB_TOKEN,
+        });
+    }
+
+    async search_issues(search_terms: string) {
+        const qualifiers = [search_terms, "is:issue", "repo:SerenityOS/serenity"];
+        const results = await this.octokit.search.issuesAndPullRequests({
+            q: qualifiers.join("+"),
+            per_page: 1,
+        });
+        const {
+            data: { items: issues },
+        } = results;
+        return issues[0];
+    }
+
+    async search_pull_requests(search_terms: string) {
+        const qualifiers = [search_terms, "is:pull-request", "repo:SerenityOS/serenity"];
+        const results = await this.octokit.search.issuesAndPullRequests({
+            q: qualifiers.join("+"),
+            per_page: 1,
+        });
+        const {
+            data: { items: pullRequests },
+        } = results;
+        return pullRequests[0];
+    }
+}
+
+const api = new GithubAPI();
+export default api;
--- src/config/secrets.ts
+++ src/config/secrets.ts
@@ -2,8 +2,14 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
 
-export const DISCORD_TOKEN = process.env["token"];
+export const DISCORD_TOKEN = process.env["discord_token"];
+export const GITHUB_TOKEN = process.env["github_token"];
 
 if (!DISCORD_TOKEN) {
     console.error("No 'discord token' provided in .env file.");
+}
+if (!GITHUB_TOKEN) {
+    console.error(
+        "No 'github token' provided in .env file, the rate limit will be greatly reduced!"
+    );
 }
