b7b921a025372acfafe47993007f25d827d53ff5
Brian Gianforcaro <bgianf@serenityos.org>
2021-06-16 01:19:01 -0700

    QuickLinksCommand: Add the ability to suppress embeds for quick links
    The link embeds take up significant visual space in discord, for a
    preview of the github repro that we all know, there is no real reason
    for it to be used for links, unless they are images / different sites.
    
    The `!soytineres` command is special as it relies on the image emebed.

--- src/commands/quickLinksCommand.ts
+++ src/commands/quickLinksCommand.ts
@@ -12,37 +12,43 @@ export class QuickLinksCommand implements Command {
     private readonly documentation: string =
         "https://github.com/SerenityOS/serenity/blob/master/Documentation";
 
-    readonly links: { help: string; response: string; name: string }[] = [
+    readonly links: { help: string; response: string; name: string; suppressEmbeds: boolean }[] = [
         {
             name: "faq",
             response: `FAQ: ${this.documentation}/FAQ.md`,
             help: "get a link to the SerenityOS FAQ",
+            suppressEmbeds: true,
         },
         {
             name: "build",
             response: `How To Build: ${this.documentation}/BuildInstructions.md`,
             help: "get a link to the build docs",
+            suppressEmbeds: true,
         },
         {
             name: "wsl",
             response: `WSL Specific Notes: ${this.documentation}/NotesOnWSL.md`,
             help: "get a link to the wsl specific notes",
+            suppressEmbeds: true,
         },
         {
             name: "install",
             response: `Installing on real hardware: ${this.documentation}/INSTALL.md`,
             help: "get a link to the directions for installing SerenityOS on real hardware",
+            suppressEmbeds: true,
         },
         {
             name: "iso",
             response: `There are no ISO images. This project does not cater to non-technical users. See the FAQ: ${this.documentation}/FAQ.md`,
             help: "Respond with the iso image policy + FAQ link",
+            suppressEmbeds: true,
         },
         {
             name: "bot-src",
             response:
                 "Bot Source: https://github.com/SerenityOS/discord-bot/tree/master/src/commands",
             help: "get a link to the source code for bot commands",
+            suppressEmbeds: true,
         },
         {
             name: "soytineres",
@@ -50,6 +56,7 @@ export class QuickLinksCommand implements Command {
             response:
                 "https://cdn.discordapp.com/attachments/830525235803586570/843838343905411142/IMG_20210517_170429.png ",
             help: "!SOytinereS ot emocleW",
+            suppressEmbeds: false,
         },
     ];
 
@@ -66,6 +73,10 @@ export class QuickLinksCommand implements Command {
     async run(parsedUserCommand: CommandParser): Promise<void> {
         const link = this.links.find(link => link.name === parsedUserCommand.parsedCommandName);
         assert(link);
-        await parsedUserCommand.send(link.response);
+        const message = await parsedUserCommand.send(link.response);
+
+        if (link.suppressEmbeds) {
+            await message.suppressEmbeds();
+        }
     }
 }
