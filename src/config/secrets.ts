47590accd386d1f0d9fffe97a5b7eca17fffd200
Linus Groh <mail@linusgroh.de>
2021-07-07 11:32:49 +0100

    Config: Use exact environment variable names in warning messages
    This makes it easier to know which names exactly it's expecting.

--- src/config/secrets.ts
+++ src/config/secrets.ts
@@ -13,15 +13,15 @@ export const GITHUB_TOKEN = process.env["github_token"];
 export let GUILD_ID = process.env["guild_id"];
 
 if (!DISCORD_TOKEN) {
-    console.error("No 'discord token' provided in .env file.");
+    console.error("No 'discord_token' provided in .env file.");
 }
 if (!GITHUB_TOKEN) {
     console.error(
-        "No 'github token' provided in .env file, the rate limit will be greatly reduced!"
+        "No 'github_token' provided in .env file, the rate limit will be greatly reduced!"
     );
 }
 if (!GUILD_ID) {
-    console.warn("No 'guild id' provided in .env file, using id of the SerenityOS guild.");
+    console.warn("No 'guild_id' provided in .env file, using id of the SerenityOS guild.");
 
     GUILD_ID = "830522505605283862";
 }
