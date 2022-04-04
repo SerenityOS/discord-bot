b20632e164e66d31e4763dc3396f67e14cf483f4
networkException <git@nwex.de>
2022-04-05 00:12:34 +0200

    Commands: Remove duplicate code for github by using existing utilities
    This patch expands on the extraction of embed generation code for pulls
    and issues introduced in 6b89272f80fe04cbcb391cb158d119779702d28d by
    porting the github command to use the shared code.

--- src/commands/githubCommand.ts
+++ src/commands/githubCommand.ts
@@ -4,24 +4,15 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-import { RestEndpointMethodTypes } from "@octokit/rest";
 import {
     ApplicationCommandOptionData,
     ChatInputApplicationCommandData,
-    ColorResolvable,
     CommandInteraction,
-    MessageEmbed,
 } from "discord.js";
 import githubAPI from "../apis/githubAPI";
+import { embedFromIssueOrPull } from "../util/embedFromIssueOrPull";
 import { getSadCaret } from "../util/emoji";
 import Command from "./command";
-
-const enum GitHubColor {
-    Open = "#57ab5a",
-    Closed = "#e5534b",
-    Merged = "#6e40c9",
-    Draft = "#768390",
-}
 
 const URL_REGEX = /.+github.com\/SerenityOS\/serenity\/(?:issues|pull)\/(\d+).*/;
 
@@ -72,7 +63,7 @@ export class GithubCommand extends Command {
         const url = interaction.options.getString("url");
 
         if (number !== null) {
-            const result = await this.embedFromIssueOrPull(await githubAPI.getIssueOrPull(number));
+            const result = await embedFromIssueOrPull(await githubAPI.getIssueOrPull(number));
 
             if (result) return await interaction.reply({ embeds: [result] });
         }
@@ -81,17 +72,14 @@ export class GithubCommand extends Command {
             const matches = url.match(URL_REGEX);
             if (matches !== null && matches[1]) {
                 const number = parseInt(matches[1]);
-                const result = await this.embedFromIssueOrPull(
-                    await githubAPI.getIssueOrPull(number)
-                );
+                const result = await embedFromIssueOrPull(await githubAPI.getIssueOrPull(number));
+
                 if (result) return await interaction.reply({ embeds: [result] });
             }
         }
 
         if (query) {
-            const result = await this.embedFromIssueOrPull(
-                await githubAPI.searchIssuesOrPulls(query)
-            );
+            const result = await embedFromIssueOrPull(await githubAPI.searchIssuesOrPulls(query));
 
             if (result) return await interaction.reply({ embeds: [result] });
         }
@@ -102,133 +90,5 @@ export class GithubCommand extends Command {
             content: `No matching issues or pull requests found ${sadcaret ?? ":^("}`,
             ephemeral: true,
         });
-    }
-
-    private async embedFromIssueOrPull(
-        issueOrPull: RestEndpointMethodTypes["issues"]["get"]["response"]["data"] | undefined
-    ): Promise<MessageEmbed | undefined> {
-        if (!issueOrPull) return undefined;
-
-        if (issueOrPull.pull_request) {
-            const pull = await githubAPI.getPull(issueOrPull.number);
-
-            if (pull) return this.embedFromPull(pull);
-        }
-
-        return this.embedFromIssue(issueOrPull);
-    }
-
-    private embedFromIssue(
-        issue: RestEndpointMethodTypes["issues"]["get"]["response"]["data"]
-    ): MessageEmbed {
-        let description = issue.body || "";
-        if (description.length > 300) {
-            description = description.slice(0, 300) + "...";
-        }
-
-        const color = issue.state === "open" ? GitHubColor.Open : GitHubColor.Merged;
-
-        const embed = new MessageEmbed()
-            .setColor(color)
-            .setTitle(`#${issue.number}: ${issue.title}`)
-            .setURL(issue.html_url)
-            .setDescription(description)
-            .addField("Type", "Issue", true)
-            .addField("Created", `<t:${new Date(issue.created_at).valueOf() / 1000}:R>`, true)
-            .addField("Comments", issue.comments.toString(), true)
-            .addField("State", issue.state === "open" ? "Open" : "Closed", true);
-
-        const labels = issue.labels
-            .map(label => (typeof label === "string" ? label : label.name))
-            .filter(name => name !== undefined)
-            .join(", ");
-
-        if (labels.length !== 0) {
-            embed.addField("Labels", labels, true);
-        }
-
-        if (issue.closed_at && issue.closed_by != null) {
-            embed.addField(
-                "Closed",
-                `<t:${new Date(issue.closed_at).valueOf() / 1000}:R> by [${
-                    issue.closed_by.login
-                }](${issue.closed_by.html_url})`,
-                true
-            );
-        }
-
-        if (issue.user != null) {
-            embed.setAuthor(`@${issue.user.login}`, issue.user.avatar_url, issue.user.html_url);
-        }
-
-        return embed;
-    }
-
-    private embedFromPull(
-        pull: RestEndpointMethodTypes["pulls"]["get"]["response"]["data"]
-    ): MessageEmbed {
-        let description = pull.body || "";
-        if (description.length > 300) {
-            description = description.slice(0, 300) + "...";
-        }
-
-        const state = new Array<string>();
-
-        let color: ColorResolvable;
-
-        if (pull.draft) state.push("Draft");
-
-        if (pull.merged) {
-            color = GitHubColor.Merged;
-
-            state.push("Merged");
-        } else if (pull.state === "closed") {
-            color = GitHubColor.Closed;
-
-            state.push("Closed");
-        } else {
-            if (pull.draft) color = GitHubColor.Draft;
-            else color = GitHubColor.Open;
-
-            state.push("Open");
-        }
-
-        const embed = new MessageEmbed()
-            .setColor(color)
-            .setTitle(`#${pull.number}: ${pull.title}`)
-            .setURL(pull.html_url)
-            .setDescription(description)
-            .addField("Type", "Pull Request", true)
-            .addField("Created", `<t:${new Date(pull.created_at).valueOf() / 1000}:R>`, true)
-            .addField("Commits", `${pull.commits} (+${pull.additions} -${pull.deletions})`, true)
-            .addField("Comments", pull.comments.toString(), true)
-            // @ts-expect-error Intl.ListFormat is not yet part of the typescript library definitions,
-            //                  see https://github.com/microsoft/TypeScript/issues/46907
-            .addField("State", new Intl.ListFormat().format(state), true);
-
-        const labels = pull.labels
-            .map(label => label.name)
-            .filter(name => name !== undefined)
-            .join(", ");
-
-        if (pull.labels.length !== 0) {
-            embed.addField("Labels", labels, true);
-        }
-
-        if (pull.merged && pull.merged_at && pull.merged_by != null) {
-            embed.addField(
-                "Merged",
-                `<t:${new Date(pull.merged_at).valueOf() / 1000}:R> by [${pull.merged_by.login}](${
-                    pull.merged_by.html_url
-                })`,
-                true
-            );
-        }
-
-        if (pull.user != null) {
-            embed.setAuthor(`@${pull.user.login}`, pull.user.avatar_url, pull.user.html_url);
-        }
-
-        return embed;
     }
 }
