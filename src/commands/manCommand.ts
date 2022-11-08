SerenityOS/discord-bot

commit 4b088e8941f7be1f4b036c96cba86771d0513702

ManCommand: Add "View on GitHub" and "View on man.serenityos.org" links
This patch adds two links to the embed description allowing to easily
navigate to either the GitHub repository to view the markdown file's
history or to the rendered preview with crosslinks on serenityos.org.

src/commands/manCommand.ts
@@ -56,11 +56,11 @@ export class ManCommand extends Command {
         const result = await githubAPI.fetchSerenityManpage(section, page);
 
         if (result) {
-            const { markdown, url } = result;
+            const { markdown, url: githubUrl } = result;
 
             await interaction.reply({
                 fetchReply: true,
-                embeds: [ManCommand.embedForMan(markdown, url, section, page, true)],
+                embeds: [ManCommand.embedForMan(markdown, githubUrl, section, page, true)],
                 components: [await ManCommand.buttons(interaction)],
             });
         } else {
@@ -96,10 +96,10 @@ export class ManCommand extends Command {
 
             if (result == null) return;
 
-            const { markdown, url, page, section } = result;
+            const { markdown, url: githubUrl, page, section } = result;
 
             interaction.update({
-                embeds: [ManCommand.embedForMan(markdown, url, section, page, collapsed)],
+                embeds: [ManCommand.embedForMan(markdown, githubUrl, section, page, collapsed)],
             });
         }
     }
@@ -126,7 +126,7 @@ export class ManCommand extends Command {
 
     static embedForMan(
         markdown: string,
-        url: string,
+        githubUrl: string,
         section: string,
         page: string,
         collapsed: boolean
@@ -141,7 +141,7 @@ export class ManCommand extends Command {
             if (line.startsWith("## ")) {
                 if (currentParagraph.content !== "") {
                     if (currentParagraph.title === "Name") {
-                        name = currentParagraph.content;
+                        name = currentParagraph.content.replace(/[\r\n]/g, "");
                     } else {
                         paragraphs.push(currentParagraph);
                     }
@@ -163,10 +163,16 @@ export class ManCommand extends Command {
             }
         }
 
+        const url = `https://man.serenityos.org/man${section}/${page}.html`;
+
         const embed = new MessageEmbed()
             .setTitle(`${page}(${section})`)
-            .setDescription(name ?? "Name not found")
-            .setURL(`https://man.serenityos.org/man${section}/${page}.html`)
+            .setDescription(
+                `${
+                    name ?? "Name not found"
+                }\n\n[View on GitHub](${githubUrl}) - [View on man.serenityos.org](${url})`
+            )
+            .setURL(url)
             .setTimestamp();
 
         for (const paragraph of paragraphs)

commit 5decb89c62fe0192d1be63ab5ce8fc2744f8b978

ManCommand: Fix min/max actions
This regressed in 4b088e8 due to changing the embed's main URL from the
GitHub one to the `man.serenity.org` one.

The solution was to instead use the GitHub link we have in the embed's
description.

src/commands/manCommand.ts
@@ -90,9 +90,11 @@ export class ManCommand extends Command {
         if (message.embeds.length === 1) {
             const embed: MessageEmbed = message.embeds[0];
 
-            if (!embed.url) return;
+            if (!embed.description) return;
 
-            const result = await githubAPI.fetchSerenityManpageByUrl(embed.url);
+            const result = await githubAPI.fetchSerenityManpageByUrl(
+                embed.description?.match(/\(([^)]+)\)/)![1]
+            );
 
             if (result == null) return;
 